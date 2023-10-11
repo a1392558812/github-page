e96bcce90676f138062caab5d32e316b1e388d8f
Awen <60727751+a1392558812@users.noreply.github.com>
2023-10-11 09:36:07 +0800

    2023/10/11

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode }) => {
+  console.log('command, mode ===>:', command, mode)
+  return {
+    base: mode === 'production' ? '/github-page' : './',
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
