4118307615747992cdc55dd319aa38cac25bffc9
Awen <60727751+a1392558812@users.noreply.github.com>
2023-10-31 17:13:27 +0800

    2023/10/31

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode }) => {
@@ -13,6 +14,18 @@ export default defineConfig(({command, mode }) => {
     plugins: [
       vue(),
       vueJsx(),
+      copy({
+        targets: [
+            { 
+                src: 'index.html', 
+                dest: 'public' ,
+                rename: (name, extension, fullPath) => {
+                    console.log('name, extension, fullPath: -->', name, extension, fullPath)
+                    return `404.html`
+                }
+            },
+        ]
+      })
     ],
     resolve: {
       alias: {
